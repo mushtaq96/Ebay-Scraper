0a03fc16ce4b113c85d7d41bd41680d9bde0acec
mushtaq96 <mushtaq96smb@gmail.com>
2023-04-10 16:29:31 +0200

    feat: add basic UI, fetch BE endpoint logic

--- src/App.js
+++ src/App.js
@@ -1,24 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import { TextField, Button, Box } from "@mui/material";
 
 function App() {
+  const [query, setQuery] = useState("");
+  const [recentQueries, setRecentQueries] = useState([]);
+
+  const handleSearch = () => {
+    // send query to the server here
+    fetch(`http://localhost:8000/query?q=${query}`)
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+      });
+    // update recent queries
+    setRecentQueries((prevRecentQueries) => [
+      query,
+      ...prevRecentQueries.slice(0, 9),
+    ]);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <Box
+      display="flex"
+      flexDirection="column"
+      alignItems="center"
+      justifyContent="center"
+      height="100vh"
+    >
+      <h1>eBay Giveaway Finder</h1>
+      <Box display="flex" alignItems="center">
+        <TextField
+          label="Enter Query"
+          value={query}
+          onChange={(e) => setQuery(e.target.value)}
+        />
+        <Button
+          variant="contained"
+          style={{ marginLeft: 8 }}
+          onClick={handleSearch}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          Subscribe
+        </Button>
+      </Box>
+      <Box>
+        <h3>Recent Queries</h3>
+        <ul>
+          {recentQueries.map((recentQuery, index) => (
+            <li key={index}>{recentQuery}</li>
+          ))}
+        </ul>
+      </Box>
+    </Box>
   );
 }
 
